40e1edda4ad67622c31220a58cba3b757a5c19fb
Andy Neff <andyneff@users.noreply.github.com>
2015-10-14 16:52:42 -0400

    Fixed a number of bugs when compiling vxl in windows
    Added Ninja Support in Windows, via setting
    -VIP_CMAKE_PLATFORM
    -VIP_CMAKE_NINJA
    -VIP_CMAKE_COMPILIER
    -VIP_VXL_BUILD_BIN_DIR
    -VIP_VXL_BUILD_LIB_DIR
    The last two are only needed because VS and ninja put them in different
    places

--- build_vxl.py
+++ build_vxl.py
@@ -6,6 +6,9 @@ from os.path import join as path_join
 from distutils.dir_util import mkpath
 import subprocess;
 
+if os.name=='nt':
+  import ntpath
+
 from ast import literal_eval
 
 import argparse
@@ -26,56 +29,68 @@ def main():
   mkpath(vxlDir);
   os.chdir(vxlDir);
 
-  if not args.rebuild:
-    if os.name == 'nt':
-      if not os.path.exists(env['VIP_GLEW_INCLUDE_DIR']):
-        print "You are missing Glew"
-        from voxel_globe.tools.wget import download
-        print "Downloading..."
-        zipFile = os.path.join(env['VIP_GLEW_INCLUDE_DIR'], '../../glew.zip')
-        dater = download('http://downloads.sourceforge.net/project/glew/glew/1.12.0/glew-1.12.0-win32.zip', 
-                         zipFile);
-        print "Unpacking..."
-        subprocess.Popen(['7z.exe', 'x', zipFile], cwd=os.path.join(env['VIP_GLEW_INCLUDE_DIR'], '../..')).wait();
-      from distutils.msvc9compiler import find_vcvarsall
-      if 'VIP_CMAKE_PLATFORM' in env:
-        platform = env['VIP_CMAKE_PLATFORM']
+
+  if os.name == 'nt':
+    if not os.path.exists(env['VIP_GLEW_INCLUDE_DIR']):
+      print "You are missing Glew"
+      from voxel_globe.tools.wget import download
+      print "Downloading..."
+      zipFile = os.path.join(env['VIP_GLEW_INCLUDE_DIR'], '../../glew.zip')
+      dater = download('http://downloads.sourceforge.net/project/glew/glew/' 
+                       '1.12.0/glew-1.12.0-win32.zip', 
+                       zipFile)
+      print "Unpacking..."
+      subprocess.Popen(['7z.exe', 'x', zipFile], 
+          cwd=os.path.join(env['VIP_GLEW_INCLUDE_DIR'], '../..')).wait();
+    from distutils.msvc9compiler import find_vcvarsall
+    platform = env.get('VIP_CMAKE_PLATFORM', None)
+    if 'VCVARSALL' not in env:
+      if platform and platform.lower().startswith('visual'):
         msvcVersion = int(platform.split(' ')[2])
         vcvarall = find_vcvarsall(msvcVersion)
       else:
-        for v in range(14, 8, -1): #Start with version 2015(14), and got back to (8), before that its 32 bit only
+        for v in range(14, 8, -1): 
+        #Start with version 2015(14), and got back to (8),
+        #before that its 32 bit only, and we don't care about that
           vcvarall = find_vcvarsall(v)
           if not vcvarall is None:
-            msvcVersion = v
-            # if v == 8:
-              # platform = 'Visual Studio 8 2005 Win64'
-            # elif v == 9:
-              # platform = 'Visual Studio 9 2008 Win64' #Pro only, or some weird express+SDK7 hybrid which is theoretically possible?
-            # else:
-            platform = 'Visual Studio %d Win64' % v
-            break;
-      env['VCCARSALL'] = vcvarall
-    else:
-      platform = env['VIP_CMAKE_PLATFORM']
+            if platform.lower() != 'ninja':
+              platform = 'Visual Studio %d Win64' % v
+            break
+      env['VCVARSALL'] = vcvarall
+  else:
+    platform = env['VIP_CMAKE_PLATFORM']
 
+  if not args.rebuild:
     cmake_options = [];
     
-    cmake_options += ['-G', platform];
+    cmake_options += ['-G', platform]
+
+    if platform.lower()=='ninja':
+      cmake_options += ['-D', 'CMAKE_MAKE_PROGRAM='+env['VIP_CMAKE_NINJA']]
+      cmake_options += ['-D', 'CMAKE_C_COMPILER='+env['VIP_CMAKE_COMPILER']]
+      cmake_options += ['-D', 'CMAKE_CXX_COMPILER='+
+                        env['VIP_CMAKE_COMPILER']]
+
+
     cmake_options += ['-D', 'CMAKE_BUILD_TYPE='+env['VIP_VXL_BUILD_TYPE']];
     
     if 'VIP_OPENCL_INCLUDE_PATH' in env:
-      cmake_options += ['-D', 'OPENCL_INCLUDE_PATH='+env['VIP_OPENCL_INCLUDE_PATH']]
+      cmake_options += ['-D', 'OPENCL_INCLUDE_PATH='+
+                        env['VIP_OPENCL_INCLUDE_PATH']]
     if 'VIP_OPENCL_LIBRARY_PATH' in env:
-      cmake_options += ['-D', 'OPENCL_LIBRARY_PATH='+env['VIP_OPENCL_LIBRARY_PATH']]
+      cmake_options += ['-D', 'OPENCL_LIBRARY_PATH='+
+                        env['VIP_OPENCL_LIBRARY_PATH']]
     if 'VIP_OPENCL_NVIDIA_LIBRARY_PATH' in env:
-      cmake_options += ['-D', 'OPENCL_NVIDIA_LIBRARY_PATH='+env['VIP_OPENCL_NVIDIA_LIBRARY_PATH']]
+      cmake_options += ['-D', 'OPENCL_NVIDIA_LIBRARY_PATH='+
+                        env['VIP_OPENCL_NVIDIA_LIBRARY_PATH']]
     if 'VIP_GLEW_INCLUDE_DIR' in env:
       cmake_options += ['-D', 'GLEW_INCLUDE_DIR='+env['VIP_GLEW_INCLUDE_DIR']]
     if 'VIP_GLEW_LIBRARY' in env:
       cmake_options += ['-D', 'GLEW_LIBRARY='+env['VIP_GLEW_LIBRARY']]
 
     if platform=="Eclipse CDT4 - Unix Makefiles":
-      cmake_options += ['-D', 'CMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=True'];
+      cmake_options += ['-D', 'CMAKE_ECLIPSE_GENERATE_SOURCE_PROJECT=True']
 
     cmake_options += ['-D', 'CMAKE_INSTALL_PREFIX='+
                    path_join(vxlDir, 'install')];
@@ -83,10 +98,14 @@ def main():
     cmake_options += ['-D', 'EXECUTABLE_OUTPUT_PATH='+
                    path_join(vxlDir, 'bin')];
 
-    cmake_options += ['-D', 'PYTHON_INCLUDE_DIR='+env['VIP_PYTHON_INCLUDE_DIR'],
-                      '-D', 'PYTHON_LIBRARY='+env['VIP_PYTHON_LIBRARY'],
-                      '-D', 'PYTHON_INCLUDE_DIR2='+env['VIP_PYTHON_INCLUDE_DIR'],
-                      '-D', 'PYTHON_EXECUTABLE='+env['VIP_PYTHON_EXECUTABLE']];
+    cmake_options += ['-D', 'PYTHON_INCLUDE_DIR='+
+                      env['VIP_PYTHON_INCLUDE_DIR'],
+                      '-D', 'PYTHON_LIBRARY='+
+                      env['VIP_PYTHON_LIBRARY'],
+                      '-D', 'PYTHON_INCLUDE_DIR2='+
+                      env['VIP_PYTHON_INCLUDE_DIR'],
+                      '-D', 'PYTHON_EXECUTABLE='+
+                      env['VIP_PYTHON_EXECUTABLE']];
                       
     tmp = env.pop('VIP_CMAKE_ECLIPSE', None);
     if tmp:
@@ -107,18 +126,39 @@ def main():
       for entry in tmp:
         cmake_options += ['-D', entry];
 
+    #Run CMake
+    if os.name=='nt':
+      if platform.lower()=='ninja':
+        args = [ntpath.normpath(os.path.join(env['VIP_INSTALL_DIR'], 
+                                             'run_vcvarsall.bat'))]
+        pid = subprocess.Popen(args+[env['VIP_CMAKE']] + cmake_options + 
+                               [env['VIP_VXL_SRC_DIR']])
+      else:
+        pid = subprocess.Popen([env['VIP_CMAKE']] + cmake_options + 
+                               [env['VIP_VXL_SRC_DIR']])
 
-    pid = subprocess.Popen([env['VIP_CMAKE']] + cmake_options + [env['VIP_VXL_SRC_DIR']]);
-    pid.wait();
+    else:
+      pid = subprocess.Popen([env['VIP_CMAKE']] + cmake_options + 
+                               [env['VIP_VXL_SRC_DIR']])
+    pid.wait()
 
+  #Make it
   if os.name=='nt':
-    import ntpath
-    args = [ntpath.normpath(os.path.join(env['VIP_INSTALL_DIR'], 'run_vcvarsall.bat')), 'devenv', 'vxl.sln', '/Build', env['VIP_VXL_BUILD_TYPE']+'^^^|x64']
-    #The ^^^ is for Stupid batch escape limitation with |
+    if platform.lower()=='ninja':
+      args = [ntpath.normpath(os.path.join(env['VIP_INSTALL_DIR'], 
+                                           'run_vcvarsall.bat')), 
+              env['VIP_CMAKE_NINJA']]
+    else:
+      args = [ntpath.normpath(os.path.join(env['VIP_INSTALL_DIR'], 
+                                           'run_vcvarsall.bat')), 
+              'devenv', 'vxl.sln', '/Build', 
+              env['VIP_VXL_BUILD_TYPE']+'^^^|x64']
+    #The ^^^ is for Stupid batch escape limitation with | Thank you windows! :(
     print "Loading vxl solution... This may take a few minutes."
     pid = subprocess.Popen(args, cwd=vxlDir);
   else:
-    pid = subprocess.Popen([env['VIP_VXL_MAKE_COMMAND'], '-j', '10'], cwd=vxlDir);
+    pid = subprocess.Popen([env['VIP_VXL_MAKE_COMMAND'], '-j', '10'], 
+                           cwd=vxlDir);
   pid.wait();
 
 if __name__=='__main__':
