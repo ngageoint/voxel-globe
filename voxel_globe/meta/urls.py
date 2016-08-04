2307f2c7200305808b71978f351832a10558f269
Andy Neff <andyneff@users.noreply.github.com>
2016-08-04 08:57:27 -0400

    Add get_additional_image_info endpoint

--- voxel_globe/meta/urls.py
+++ voxel_globe/meta/urls.py
@@ -5,6 +5,9 @@ urlpatterns = patterns('',
 # json API calls    
     url(r'^fetchTiePoints$', views.fetchTiePoints, name='fetchTiePoints'),
     url(r'^fetch_voxel_world_bbox/(?P<voxel_world_id>\d+)$', views.fetch_voxel_world_bounding_box, name='fetch_voxel_world_bbox'),
+
+    url(r'^get_additional_image_info/(?P<image_id>\d+)/(?P<camera_set_id>\d+)$', views.get_additional_image_info, name='get_additional_image_info'),
+
     
 #   modifications to data
     url(r'^createTiePoint$', views.createTiePoint, name='createTiePoint'),
