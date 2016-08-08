81eb9a284a25dbfc6eaaf954ea6011a072edcb85
Andy Neff <andyneff@users.noreply.github.com>
2016-08-08 14:49:45 -0400

    Added vxl msi to grey functionality
    -Use new vxl process in event trigger task
    -Re-fix event trigger form again

--- voxel_globe/event_trigger/forms.py
+++ voxel_globe/event_trigger/forms.py
@@ -2,6 +2,6 @@ from django import forms
 import voxel_globe.meta.models as models
 
 class EventTriggerForm(forms.Form):
-  sites = models.SattelSite.objects.filter(satteleventtrigger__isnull=False) \
+  sites = models.SattelSite.objects.exclude(satteleventtrigger=None) \
       .order_by('name')
   site = forms.ModelChoiceField(label="Site", queryset=sites)
