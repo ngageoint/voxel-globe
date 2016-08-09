8f3bafdacbfcea449a811b5e34a432f0af8c38ca
Andy Neff <andyneff@users.noreply.github.com>
2016-08-09 14:24:41 -0400

    Revert "Disabled up is up fake values"
    This reverts commit 01fd3aface160e5d9b0317d5272b9eeb0f732c6f.

--- voxel_globe/meta/views.py
+++ voxel_globe/meta/views.py
@@ -133,5 +133,6 @@ def get_additional_image_info(request, image_id, camera_set_id):
     print vpgl.rational_camera_rotate_to_north(vxl_cam)
     print 2
     response['north_rotation'] = vpgl.rational_camera_rotate_to_north(vxl_cam)
-  
+  response['up_rotation'] = 1.1
+
   return HttpResponse(json.dumps(response))
