ef3d695114986c5cac23de48317c0dbdd30a53fe
Andy Neff <andyneff@users.noreply.github.com>
2016-09-12 16:17:03 -0400

    Flatten tiepoint_error

--- voxel_globe/tiepoint_registration/urls.py
+++ voxel_globe/tiepoint_registration/urls.py
@@ -5,6 +5,10 @@ from django.conf.urls import patterns, url
 import voxel_globe.tiepoint_registration.views as views
 
 urlpatterns = patterns('',
-    url(r'^$', views.make_order_1, name='make_order_1'),
-    url(r'^order/(?P<image_set_id>\d+)$', views.make_order_2, name="make_order_2"),
+    url(r'^tiepoint_error$', views.tiepoint_error_1, name='tiepoint_error_1'),
+    url(r'^tiepoint_error/(?P<image_set_id>\d+)$', views.tiepoint_error_2, name="tiepoint_error_2"),
+    url(r'^tiepoint_error/(?P<image_set_id>\d+)/(?P<scene_id>\d+)$', views.tiepoint_error_3, name="tiepoint_error_3"),
+    url(r'^status/(?P<task_id>\d+)$', views.order_status, name="order_status"),
+    url(r'^tiepoint_registration$', views.tiepoint_registration_1, name='tiepoint_registration_1'),
+    url(r'^tiepoint_registration/(?P<image_set_id>\d+)$', views.tiepoint_registration_2, name="tiepoint_registration_2"),
     )
--- voxel_globe/tiepoint_registration/views.py
+++ voxel_globe/tiepoint_registration/views.py
@@ -2,19 +2,50 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from django.template import RequestContext, loader
 
-def make_order_1(request):
+def tiepoint_registration_1(request):
   from voxel_globe.meta import models
   image_set_list = models.ImageSet.objects.all()
-  return render(request, 'tiepoint_registration/html/make_order_1.html', 
+  return render(request, 'tiepoint_registration/html/tiepoint_registration_1.html', 
                 {'image_set_list':image_set_list})
 
-def make_order_2(request, image_set_id):
+def tiepoint_registration_2(request, image_set_id):
   from voxel_globe.tiepoint_registration import tasks
 
   image_set_id = int(image_set_id)
   
   t = tasks.tiepoint_registration.apply_async(args=(image_set_id,), user=request.user)
   
-  return render(request, 'tiepoint_registration/html/make_order_2.html',
+  return render(request, 'tiepoint_registration/html/tiepoint_registration_2.html',
                 {'task_id': t.task_id})
   
+def tiepoint_error_1(request):
+  from voxel_globe.meta import models
+  image_set_list = models.ImageSet.objects.all()
+  return render(request, 'order/tiepoint_error_calculation/html/tiepoint_error_1.html', 
+                {'image_set_list':image_set_list})
+
+def tiepoint_error_2(request, image_set_id):
+  from voxel_globe.meta import models
+  scene_list = models.Scene.objects.all()
+  return render(request, 'order/tiepoint_error_calculation/html/tiepoint_error_2.html',
+                {'scene_list':scene_list,
+                 'image_set_id':image_set_id})
+
+def tiepoint_error_3(request, image_set_id, scene_id):
+  from voxel_globe.tiepoint_registration import tasks
+
+  image_set_id = int(image_set_id)
+  
+  t = tasks.tiepoint_error_calculation.apply_async(args=(image_set_id, scene_id),
+                                                   user=request.user)
+  
+  return render(request, 'order/tiepoint_error_calculation/html/tiepoint_error_3.html',
+                {'task_id': t.task_id})
+  
+def order_status(request, task_id):
+  from celery.result import AsyncResult
+  
+  task = AsyncResult(task_id)
+
+  return render(request, 'task/html/task_3d_error_results.html',
+                {'task': task})
